cba992c47bc6416c4538f954d45beae8ac586708
Rafael <rafaelfariasprogramador@gmail.com>
2023-01-04 18:59:29 -0300

    Populando o banco de dados

--- backend/src/main/java/com/garosuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/garosuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.garosuperior.dscatalog.entities.Category;
 import com.garosuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,7 +12,7 @@ import java.util.List;
 public class CategoryService {
     @Autowired
     CategoryRepository repository;
-
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return repository.findAll();
     }
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into tb_category (name) values ('Book')
+insert into tb_category (name) values ('Electronics')
+insert into tb_category (name) values ('Computers')
+
